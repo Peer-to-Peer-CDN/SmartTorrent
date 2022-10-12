a1d68b5c8fbc7bc08379ca3a3131f01e58ade4c1
Adrian Locher <locher-adrian@hotmail.com>
2022-10-12 12:33:37 +0200

    add blacklist contract

---
+++ contracts/blacklist.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract TorrentBlackList {
+    address private ownerContract;
+
+    constructor (address _ownerContract) {
+        ownerContract = _ownerContract;
+    }
+
+    enum EntryCategory {
+        NOTLISTED, MALWARE, COPYRIGHTED, MALEWAREANDCOPYRIGHT
+    }
+
+    mapping (bytes32 => EntryCategory) entries;
+
+    function addCategoryToEntry(bytes32 hash, EntryCategory category) external {
+        require(msg.sender == ownerContract);
+
+        uint8 currentCat = uint8(entries[hash]);
+        uint8 newCat = uint8(category);
+
+        require(newCat + currentCat < 4); //max is MALEWAREANDCOPYRIGHT
+        require(currentCat < 3); //max is MALEWAREANDCOPYRIGHT
+        require(newCat != currentCat); //cannot apply category that is already applied
+            
+        entries[hash] = EntryCategory(currentCat + newCat);
+    }
+
+    function getEntry(bytes32 hash) external view returns (EntryCategory category) {
+        return entries[hash];
+    }
+}
